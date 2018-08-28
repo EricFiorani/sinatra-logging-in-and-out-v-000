3b37d41865c66b32ee0f4ef258fad289e136f0b9
Eric Fiorani <efiorani305@gmail.com>
2018-08-28 18:47:49 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,7 +5,7 @@ class Helpers
     @user
   end
 
-  def self.is_logged_in
-    id: session[:user_id] ? true : false
+  def self.is_logged_in?(session)
+    @user = User.find_by(id: session[:user_id])
   end
 end
