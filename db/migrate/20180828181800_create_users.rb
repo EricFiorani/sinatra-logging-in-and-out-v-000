5bfc2957348fa05727db92fc2a9d123e4c6bdf52
Eric Fiorani <efiorani305@gmail.com>
2018-08-28 18:22:41 +0000

    automatically backed up by learn

--- db/migrate/20180828181800_create_users.rb
+++ db/migrate/20180828181800_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :passsword
+      t.string :password
       t.integer :balance
     end
   end
