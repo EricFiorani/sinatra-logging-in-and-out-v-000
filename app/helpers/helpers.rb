80e53106e73fa17d3016e98ba67e717ecf8927bc
Eric Fiorani <efiorani305@gmail.com>
2018-08-28 18:43:21 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
 
-  def current_user(session[:user_id])
-    
+  def current_user(session)
+    @user = User.find_by(username: params["username"], password: params["password"])
   end
 
   def is_logged_in
