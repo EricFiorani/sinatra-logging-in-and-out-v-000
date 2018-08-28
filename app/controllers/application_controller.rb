854852bef5a86d57fef07d735c8eef3eb6dfd03c
Eric Fiorani <efiorani305@gmail.com>
2018-08-28 18:38:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
       session[:id] = @user.id
       redirect '/account'
     end
-    redirect '/index'
+    redirect '/error'
   end
 
   get '/account' do
